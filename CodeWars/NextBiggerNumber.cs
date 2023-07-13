b121b23ac19e447dab884b7b391c950c381fcfd3
Louis Waweru <louis@waweru.me>
2023-07-13 02:37:29 -0400

    Instead of swap, move smaller number to he end.

--- CodeWars/NextBiggerNumber.cs
+++ CodeWars/NextBiggerNumber.cs
@@ -7,13 +7,14 @@ public partial class Kata
         char[] characters = n.ToString().ToCharArray();
         int digits = characters.Length;
 
-        for (int d = 0; d < digits; d++)
+        for (int d = 0; d <= digits; d++)
             for (int i = characters.Length - 1; i >= 1; i--)
                 if (characters[i] > characters[i - 1])
                 {
                     char temp = characters[i - 1];
                     characters[i - 1] = characters[i];
-                    characters[i] = temp;
+                    characters[i] = characters[characters.Length - 1];
+                    characters[characters.Length - 1] = temp;
 
                     return Convert.ToInt64(new string(characters));
                 }
